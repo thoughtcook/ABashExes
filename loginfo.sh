672331b622d256d370ee2d69879df8b958d4ba12
ThoughtCook <johnnyat2010@gmail.com>
2013-10-24 19:52:56 +0800

    Add more info, and remove last loginfo.log file when exec this script

--- loginfo.sh
+++ loginfo.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 echo "Record Date and time, the online users, and uptime."
 
+rm -fr loginfo.log
+
+echo "==================date&time=====================" >> loginfo.log
 date >> loginfo.log
+
+echo >> loginfo.log
+
+echo "=============The users online====================" >> loginfo.log
 
 who >> loginfo.log
 
+echo >> loginfo.log
+
+echo "=============The system uptime===================" >> loginfo.log
 uptime >> loginfo.log
 
