c4a2d4f48094bdaa924b5bac0e7c52d30e1bdf7d
ThoughtCook <johnnyat2010@gmail.com>
2013-10-30 08:59:15 +0800

    Commit again, add for loop

--- FindLargestFile.sh
+++ FindLargestFile.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
 # Find the largest file in a directory using a recrusive way.
 
-FILEDIR=/usr/src/linux/Documentation
+FILEDIR=/usr/src/linux
+TempFile=~/Documents/temp.txt
 
 cd $FILEDIR
 
-ls -l | awk '{print $5, $9}' | sort -n -r | head -n 5
+for file in *
+do
+ ls -l "$file" >> $TempFile
+done
+
+awk '{print $5, $9}' << $TempFile | sort -n -r | head -n 5
+
+#rm -f temp.txt
