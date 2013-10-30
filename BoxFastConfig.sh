a046280ab46a0d1db3889bcd4ed8961c25b7786b
Johnny <thoughtcook@163.com>
2013-10-30 10:39:07 +0800

    Modify BoxFastConfig.sh

--- BoxFastConfig.sh
+++ BoxFastConfig.sh
@@ -3,18 +3,27 @@
 
 TFTP_DIR=/home/tftpboot/
 ROOTDISK_DIR=/home/rootdisk/
-TFTP_KERNEL_NAME=vip2952.bin
-ROOTDISK=rootdisk-2952
+TFTP_KERNEL_NAME=vip2853.bin
+ROOTDISK=rootdisk-2853
 
-BOOTIMAGEDIR=/home/johnny/branch/KREATV-18198-tr069bugfix/products/ip-stb/boot_image/trunk-test/bcm45/vip29x2
+BOOTIMAGEDIR=/home/johnny/branch/KREATV-18198-tr069bugfix/products/ip-stb/boot_image/trunk-test/st40/vip28x3
+KernelFile=kreatv-kernel-nfs-trunk-test_KREATV-18198-tr069bugfix_st40_vip28x3
+RootDiskTgz=kreatv-rootdisk-trunk-test_KREATV-18198-tr069bugfix_st40_vip28x3.tgz
 
-rm -fr ($TFTPDIR)$TFTP_KERNEL_NAME
-rm -fr ($ROOTDISKDIR)$ROOTDISK
-cd /home/johnny/branch/KREATV-18198-tr069bugfix/products/ip-stb/boot_image/trunk-test/bcm45/vip29x2
-cp kreatv-kernel-nfs-trunk-test_KREATV-18198-tr069bugfix_bcm45_vip29x2 /home/tftpboot/
-cp kreatv-rootdisk-trunk-test_KREATV-18198-tr069bugfix_bcm45_vip29x2.tgz /home/rootdisk/
-cd /home/tftpboot
-mv kreatv-kernel-nfs-trunk-test_KREATV-18198-tr069bugfix_bcm45_vip29x2 vip2952.bin
-cd /home/rootdisk
-tar xvf kreatv-rootdisk-trunk-test_KREATV-18198-tr069bugfix_bcm45_vip29x2.tgz
-mv rootdisk rootdisk-2952
+#Remove last kernel and rootdisk if have
+cd $TFTPDIR
+rm -fr $TFTP_KERNEL_NAME
+cd $ROOTDISKDIR
+rm -fr $ROOTDISK
+
+#change $PWD to BOOTIMAGE directory to copy kernel and rootdisk 
+cd $BOOTIMAGEDIR
+cp $RootDiskTgz $ROOTDISK_DIR
+cp $KernelFile $TFTP_DIR
+
+#Make a few changes then
+cd $TFTP_DIR
+mv $KernelFile $TFTP_KERNEL_NAME
+cd $ROOTDISK_DIR
+tar xvf $RootDiskTgz
+mv rootdisk $ROOTDISK
