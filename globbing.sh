e90555c49b6ea4234dc9101f05a77e91cbb780a5
ThoughtCook <johnnyat2010@gmail.com>
2013-10-30 08:55:06 +0800

    add globbing.sh, Modify FindLargestFile.sh

---
+++ globbing.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# list-glob.sh: Generating [list] in a for-loop, using "globbing" ...
+# Globbing = filename expansion.
+
+GlobDir=/usr/src/linux/samples
+
+echo
+
+cd $GlobDir
+
+for file in *
+#           ^  Bash performs filename expansion
+#+             on expressions that globbing recognizes.
+do
+  ls -l "$file"  # Lists all files in $PWD (current directory).
+  #  Recall that the wild card character "*" matches every filename,
+  #+ however, in "globbing," it doesn't match dot-files.
+
+  #  If the pattern matches no file, it is expanded to itself.
+  #  To prevent this, set the nullglob option
+  #+   (shopt -s nullglob).
+  #  Thanks, S.C.
+done
