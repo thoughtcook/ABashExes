5145867c13045b865b642fe5862c8434f28fa260
ThoughtCook <johnnyat2010@gmail.com>
2013-11-04 19:34:28 +0800

    add 579.sh

--- globbing.sh
+++ globbing.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # list-glob.sh: Generating [list] in a for-loop, using "globbing" ...
 # Globbing = filename expansion.
+#Can only list file in the present directory right now, no recrusive.
 
 GlobDir=/usr/src/linux/samples
 
@@ -12,7 +13,8 @@ for file in *
 #           ^  Bash performs filename expansion
 #+             on expressions that globbing recognizes.
 do
-  ls -l "$file"  # Lists all files in $PWD (current directory).
+  echo $file
+  ls -lh $file  # Lists all files in $PWD (current directory).
   #  Recall that the wild card character "*" matches every filename,
   #+ however, in "globbing," it doesn't match dot-files.
 
